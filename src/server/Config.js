535abb917993edda78b6f8d504fd8ffb90cb37a3
stimedia-utvikling <59338113+stimedia-utvikling@users.noreply.github.com>
2021-08-13 09:08:47 +0200

    deploy

--- src/server/Config.js
+++ src/server/Config.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var Config = {
-    apiKey: "AIzaSyCbjW2dMJRvEnPJ1383StNsyAP_C0D7HDg",
-    authDomain: "free-travel-a7378.firebaseapp.com",
-    projectId: "free-travel-a7378",
-    storageBucket: "free-travel-a7378.appspot.com",
-    messagingSenderId: "918473356840",
-    appId: "1:918473356840:web:6021c89b428b2d53da271d",
-    measurementId: "G-YZPG6P5R31"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId:process.env.REACT_APP_MEASUREMENT_ID
 };
         // Initialize Firebase
 firebase.initializeApp(Config);
